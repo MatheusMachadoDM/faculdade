01f49077dd8c96761337126f6370f7791c6c9097
MatheusMachadoDM <165315314+MatheusMachadoDM@users.noreply.github.com>
2024-10-18 09:37:48 -0300

    começo da atividade extra

---
+++ Algoritmos de Programação/aula007/atividades/atividadeEXTRA/main.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+#include <stdbool.h>
+
+/*A origem dos quadrados mágicos não é conhecida, mas os antigos chineses, hindús e árabes foram os primeiros a trabalhar com os quadrados mágicos. 
+O exemplo mais antigo é o Loh-Shu encontrado na China, trata-se de um quadrado mágico de ordem 3 que data de 2850 a.C.
+
+Um quadrado mágico é uma matriz quadrada de números inteiros positivos em que a soma de cada linha, de cada coluna, 
+da diagonal principal e da diagonal secundária tem o mesmo valor M, chamado de constante mágica. Note que nenhum número pode ser repetido
+ em um quadrado mágico. Veja alguns exemplos:
+ 
+ Exemplo 1:
+ 
+ 	+-----+-----+-----+
+	|  8  |  1  |  6  |
+	+-----+-----+-----+
+	|  3  |  5  |  7  |
+	+-----+-----+-----+
+	|  4  |  9  |  2  |
+	+-----+-----+-----+
+	
+Exemplo 2:
+	
+	+-----+-----+-----+
+	|  8  |  3  |  4  |
+	+-----+-----+-----+
+	|  1  |  5  |  9  |
+	+-----+-----+-----+
+	|  6  |  7  |  2  |
+	+-----+-----+-----+
+ 
+ Exemplo 3:
+ 
+	+-----+-----+-----+
+	|  2  |  9  |  4  |
+	+-----+-----+-----+
+	|  7  |  5  |  3  |
+	+-----+-----+-----+
+	|  6  |  1  |  8  |
+	+-----+-----+-----+
+ 
+ Com base no exposto, escreva um programa que verifique se uma matriz de ordem 3 preenchida é um quadrado mágico ou não.
+ */
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL,"portuguese");
+	
+	int matriz[3][3] = {{2,9,4},{7,5,3},{6,1,8}}, i, j, vetor[8]= {{15,15,15,15,15,15,15,15}};
+	
+	for(i = 0; i < 3; i++){
+		
+		for(j = 0; j < 3; j++){
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
