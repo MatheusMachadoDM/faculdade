672429cf91545fed9b7e05bbe2eed4b5b45ea3fe
Matheus Machado <machadomatheus2906@gmail.com>
2024-10-29 11:53:41 -0300

    Falta terminar a 4

--- Algoritmos de Programação/aula009 - registros/atividades/atividade004/main.c
+++ Algoritmos de Programação/aula009 - registros/atividades/atividade004/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
 
 /*4. Crie uma estrutura com o código e a quantidade em estoque de 5 produtos. Em seguida, leia um
 pedido composto por um código de produto e quantidade. Se houver quantidade suficiente para
@@ -15,8 +16,9 @@ struct Produto{
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL,"portuguese");
 	
-	struct Produto produto[3] = {{123, 10}, {321, 5}, {213, 20}};
-	int codigo, quantidade, i;
+	struct Produto produto[5] = {{123, 10}, {321, 5}, {213, 20}, {312, 15}, {132, 20}};
+	int codigo, quantidade, i, quantidadeAtualizada;
+	bool estoque;
 	
 	printf("Digite o codigo do produto que deseja atualizar: ");
 	scanf("%d", &codigo);
@@ -24,14 +26,23 @@ int main(int argc, char *argv[]) {
 	printf("Digite a quantidade que deseja retirar do estoque: ");
 	scanf("%d", &quantidade);
 	
-	for(i = 0; i < 3; i++){
+	for(i = 0; i < 5; i++){
 		if(codigo == produto[i].codigo){
-			printf("Codigo correto");
-			return 0;
+			if(quantidade <= produto[i].quantidade){
+				produto[i].quantidade -= quantidade;
+				estoque = true;
+				quantidadeAtualizada = produto[i].quantidade;
+				i = 5;
+			}
 		}else{
-			printf("Codigo incorreto");
+			estoque = false;
 		}
 	}
 	
+	if(estoque == true){
+		printf("Estoque atualizado! Quantidade em estoque após atualização: %d", quantidadeAtualizada);
+	}else{
+		printf("Quantidade insuficiente em estoque!");
+	}
 	return 0;
 }
