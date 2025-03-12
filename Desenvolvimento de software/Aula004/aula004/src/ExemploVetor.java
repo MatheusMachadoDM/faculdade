55602e47eb7163944c95cec4829485a612662291
MatheusMachadoDM <165315314+MatheusMachadoDM@users.noreply.github.com>
2025-03-12 09:33:03 -0300

    Exercicios de vetor

---
+++ Desenvolvimento de software/Aula004/aula004/src/ExemploVetor.java
@@ -0,0 +1,11 @@
+
+public class ExemploVetor{
+    public static void main(String[] args) {
+        int vetor[] = {10, 20, 30, 40, 50};
+
+        System.out.println("Elementos do vetor: ");
+        for(int i = 0; i < vetor.length; i ++){
+            System.out.println("Índice " + i + ": " + vetor[i]);
+        }
+    }
+}
