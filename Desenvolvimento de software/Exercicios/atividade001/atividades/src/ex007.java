98626875b7da4b341d4eb35368549bb9f2ad6738
MATHEUS MACHADO DA MOTA <matheus.machado03@cs.up.edu.br>
2025-03-13 10:04:55 -0300

    ex 7

--- Desenvolvimento de software/Exercicios/atividade001/atividades/src/ex007.java
+++ Desenvolvimento de software/Exercicios/atividade001/atividades/src/ex007.java
@@ -1,25 +1,22 @@
-/*7. Crie um programa que simula um caixa eletrônico. O usuário informa o valor a ser sacado, 
-e o programa retorna quantas cédulas de cada valor serão fornecidas (100, 50, 20, 10, 5, 2, 1).
-
-Exemplo de Entrada:
-
-Digite o valor do saque: 289
-
-Saída Esperada:
-
-Notas de 100: 2
-Notas de 50: 1
-Notas de 20: 1
-Notas de 10: 1
-Notas de 5: 1
-Notas de 2: 2*/
-
 import java.util.Scanner;
 
-public class ex007{
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		
-	}
+public class ex007 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor do saque: ");
+        int valor = scanner.nextInt();
+
+        int[] cedulas = {100, 50, 20, 10, 5, 2, 1};
+
+        for (int i = 0; i < cedulas.length; i++) {
+            int cedula = cedulas[i];
+            int quantidade = valor / cedula;
+            if(quantidade > 0){
+                System.out.println("Notas de " + cedula + ": " + quantidade);
+            }
+            valor = valor % cedula;
+        }
+        scanner.close();
+    }
 }
---
+++ Desenvolvimento de software/Exercicios/atividade001/atividades/src/ex008.java
@@ -0,0 +1,20 @@
+/*8. Crie um método chamado contarVogais(String palavra) que recebe uma palavra e retorna a quantidade de vogais (a, e, i, o, u) nela.
+
+Exemplo de Entrada:
+
+Digite uma palavra: programacao
+
+Saída Esperada:
+A palavra contém 5 vogais. */
+import java.util.Scanner;
+
+public class ex008 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+    public static String contarVogais(String palavra){
+        return a;
+    }
+}
